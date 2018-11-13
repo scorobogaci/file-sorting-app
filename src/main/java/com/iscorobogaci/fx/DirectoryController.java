f92746fd05284f2e3192dc8f3336208315e0af15
iscorobogaci <Endava_9900>
2018-11-13 12:15:16 +0200

    Added alert handler - validation Added css file for design

--- src/main/java/com/iscorobogaci/fx/DirectoryController.java
+++ src/main/java/com/iscorobogaci/fx/DirectoryController.java
@@ -1,13 +1,21 @@
 package com.iscorobogaci.fx;
 
+import com.iscorobogaci.FileExceptionHandler;
+import com.iscorobogaci.FileSorter;
+import com.iscorobogaci.enums.SortingBy;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
 
 public class DirectoryController {
 
@@ -15,16 +23,20 @@ public class DirectoryController {
     private AnchorPane anchorId;
 
     @FXML
-    private TextField sourceFolderTextAreaId;
+    private TextField textAreaSourceFolder;
 
     @FXML
-    private TextField textFieldDestinationFolderId;
+    private TextField textAreaDestinationFolder;
+
+    @FXML
+    private ToggleGroup radioGroup;
+
 
     private DirectoryChooser directoryChooser;
-
     private File sourceFolder;
-
     private File destinationFolder;
+    private SortingBy sortingBy;
+
 
     public DirectoryController() {
         directoryChooser = new DirectoryChooser();
@@ -34,10 +46,10 @@ public class DirectoryController {
     private void handleSourceFolderButton(ActionEvent actionEvent) {
         Stage stage = (Stage) anchorId.getScene().getWindow();
         File file = directoryChooser.showDialog(stage);
-        sourceFolderTextAreaId.setEditable(false);
+        textAreaSourceFolder.setEditable(false);
         if (file != null) {
             this.sourceFolder = file;
-            sourceFolderTextAreaId.setText(file.getAbsolutePath());
+            textAreaSourceFolder.setText(file.getAbsolutePath());
         }
     }
 
@@ -45,16 +57,63 @@ public class DirectoryController {
     private void handleDestinationFolderButton(ActionEvent actionEvent) {
         Stage stage = (Stage) anchorId.getScene().getWindow();
         File file = directoryChooser.showDialog(stage);
-        textFieldDestinationFolderId.setEditable(false);
+        textAreaDestinationFolder.setEditable(false);
         if (file != null) {
             this.destinationFolder = file;
-            textFieldDestinationFolderId.setText(file.getAbsolutePath());
+            textAreaDestinationFolder.setText(file.getAbsolutePath());
         }
     }
 
     @FXML
-    private void handleStartButton(ActionEvent actionEvent){
-        System.out.println(sourceFolder.toPath());
-        System.out.println(destinationFolder.toPath());
+    private void handleStartButton(ActionEvent actionEvent) {
+        if (isSourceFolderNotSelected() || isDestinationFolderNotSelected()) {
+            raiseAlert();
+        } else {
+            FileSorter fileSorter = new FileSorter(sourceFolder.toPath(), destinationFolder.toPath(), sortingBy);
+            try {
+                fileSorter.copy();
+                openDestinationDirectoryInExplorer();
+            } catch (IOException e) {
+                FileExceptionHandler.getInstance().logException(e);
+            }
+            clearTextAreaFields();
+        }
     }
+
+
+    @FXML
+    public void handleToggleButton(ActionEvent actionEvent) {
+        RadioButton selectedToggle = (RadioButton) radioGroup.getSelectedToggle();
+        this.sortingBy = SortingBy.valueOf(selectedToggle.getText());
+    }
+
+    private void openDestinationDirectoryInExplorer() {
+        try {
+            Desktop.getDesktop().open(destinationFolder);
+        } catch (IOException e) {
+            FileExceptionHandler.getInstance().logException(e);
+        }
+    }
+
+    private boolean isSourceFolderNotSelected() {
+        return textAreaSourceFolder.getText().isEmpty();
+    }
+
+    private boolean isDestinationFolderNotSelected() {
+        return textAreaDestinationFolder.getText().isEmpty();
+    }
+
+    private void raiseAlert() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information");
+        alert.setHeaderText("We need your input to process the files :)");
+        alert.setContentText("Please select source folder and output folder.");
+        alert.showAndWait();
+    }
+
+    private void clearTextAreaFields() {
+        textAreaSourceFolder.clear();
+        textAreaDestinationFolder.clear();
+    }
+
 }
--- src/main/java/com/iscorobogaci/fx/Main.java
+++ src/main/java/com/iscorobogaci/fx/Main.java
@@ -9,14 +9,14 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("File-Sort");
+        primaryStage.setTitle("FSORT");
         primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
         primaryStage.show();
 
     }
-
 
     public static void main(String[] args) {
         launch(args);
